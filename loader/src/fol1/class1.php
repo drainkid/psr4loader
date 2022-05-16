383ac36fa510f1124b50bae826762f6910151ba7
drainkid <91513587+drainkid@users.noreply.github.com>
2022-05-16 12:21:25 +1000

    Add files via upload

---
+++ loader/src/fol1/class1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace fol1;
+
+class class1
+{
+
+    public function print_mes() {
+        echo "ауе ". get_class($this);
+
+    }
+
+}
---
+++ loader/src/fol1/fol2/class2.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace fol1\fol2;
+class class2
+{
+
+public function print_shit() {
+
+    echo date("l")."</br>".get_class($this);
+
+}
+}
